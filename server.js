6539e12fa5b917105aab603e36107bc8e227ca7a
Daniela Carey <danielafcarey@gmail.com>
2018-06-20 08:12:20 -0600

    Add /json endpoint to return name object

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express();
 
 app.use(express.static('public'))
 
-app.get('/', (request, response) => {
-
-});
-
 app.listen(3000, () => {
   console.log('Express intro running on localhost:3000');
 });
+
+app.get('/', (request, response) => {});
+
+app.get('/json', (request, response) => {
+  response.status(200).json({ 'name': 'Daniela' })
+})
+
+
