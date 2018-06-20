92e1c5cab3c9e93b572966cc3372cb7998bd51a0
Daniela Carey <danielafcarey@gmail.com>
2018-06-20 08:33:48 -0600

    Add extension #3 for importing data from another file into server.js

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const app = express();
+const mockData = require('./mockData.js');
 
-app.use(express.static('public'))
-
-app.listen(3000, () => {
-  console.log('Express intro running on localhost:3000');
-});
-
-app.get('/', (request, response) => {});
 
 const urlLogger = (request, response, next) => {
   console.log('Request URL:', request.url);
@@ -19,8 +13,17 @@ const timeLogger = (request, response, next) => {
   next();
 }
 
-app.get('/json', urlLogger, timeLogger, (request, response) => {
-  response.status(200).json({ 'name': 'Daniela' })
+app.use(urlLogger, timeLogger);
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {});
+
+app.get('/json', (request, response) => {
+  response.status(200).json(mockData)
+});
+
+app.listen(3000, () => {
+  console.log('Express intro running on localhost:3000');
 });
 
 
