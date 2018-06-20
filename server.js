f7e35e882904837a79b5cb208324a7a7ab36ea27
Daniela Carey <danielafcarey@gmail.com>
2018-06-20 08:07:55 -0600

    Setup static assets

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'))
+
 app.get('/', (request, response) => {
-  response.send('hello world');
+
 });
 
 app.listen(3000, () => {
